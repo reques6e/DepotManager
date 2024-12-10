17b39e30ad1914c371bdca5e2fdca5c973e66d8a
reques6e <voronin99borja61275y@rambler.ru>
2024-12-10 22:14:50 +0300

    Обновил роутеры

--- api/account/router.py
+++ api/account/router.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, status, Depends, Query
+from fastapi import APIRouter, Depends, Query, HTTPException, status
 from fastapi.responses import JSONResponse
 
-from src.auth import security
+from authx import RequestToken
+from src.auth import security, get_current_token
 
 router = APIRouter(
     prefix='/account',
@@ -11,32 +12,38 @@ router = APIRouter(
 @router.get(
     path='/authorization',
     status_code=status.HTTP_200_OK,
-    description='Авторизация по логину и хешу пароля'
+    description='Авторизация по логину и хешу пароля',
+    dependencies=[
+        Depends(security.get_token_from_request)
+    ] 
 )
-@security.auth_required
 async def GetAccount(
-    user=Depends(security.get_current_user),
+    current_user: RequestToken = Depends(get_current_token), 
     target_user: int | None = Query(
-        default=None, 
-        description='ID целевого пользователя'
+        default=None,
+        description='ID целевого пользователя (для администраторов или пользователей, имеющих права)'
     )
 ) -> JSONResponse:
     """
     Получаем данные пользователя по токену.
 
-    :param user: Данные текущего авторизованного пользователя.
-    :param target_user: ID целевого пользователя (для администраторов или пользователей имеющих права).
+    :param current_user: Данные текущего авторизованного пользователя, извлечённые из токена.
+    :param target_user: ID целевого пользователя (для администраторов или пользователей, имеющих права).
     :return: Данные текущего пользователя или информация о целевом пользователе.
     """
+
+    # Если передан target_user, добавляем дополнительную логику
     if target_user:
-        # Логика обработки целевого пользователя
         return JSONResponse(
             status_code=status.HTTP_200_OK,
-            content={"message": "Доступ разрешён к целевому пользователю", "target_user_id": target_user}
+            content={
+                "message": "Доступ разрешён к целевому пользователю",
+                "target_user_id": target_user
+            }
         )
 
-    # Возврат данных текущего пользователя
+    # Возвращаем данные текущего пользователя
     return JSONResponse(
         status_code=status.HTTP_200_OK,
-        content={"message": "Доступ разрешён", "user": user}
+        content={"message": "Доступ разрешён", "user": current_user}
     )
