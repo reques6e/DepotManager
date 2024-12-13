84c3ea00cac354b49fa39043a4cf5d4776c3b899
reques6e <voronin99borja61275y@rambler.ru>
2024-12-13 18:01:13 +0300

    Добавил макет 2FA в менеджер

--- src/manager.py
+++ src/manager.py
@@ -6,3 +6,27 @@
 # запускаем основные компоненты
 # запускаем асинхронные функцию работающие в отдельном потоке
 # ...
+
+
+# Делаем 2FA
+# import time 
+# import pyotp 
+# import qrcode 
+  
+# from src.config import config
+
+# key = config.SECRET_KEY
+  
+# uri = pyotp.totp.TOTP(key).provisioning_uri( 
+#     name='Dwaipayan_Bandyopadhyay', 
+#     issuer_name='GeeksforGeeks') 
+  
+# print(uri) 
+  
+  
+# qrcode.make(uri).save("qr.png") 
+    
+# totp = pyotp.TOTP(key) 
+  
+# while True: 
+#   print(totp.verify(input(("Enter the Code : "))))
