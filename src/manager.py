9a93395d020e45de1bac8eebf3b78f3c326890f0
reques6e <voronin99borja61275y@rambler.ru>
2024-12-14 17:16:24 +0300

    Сделал TwoFactor (Google 2FA)

--- src/manager.py
+++ src/manager.py
@@ -7,26 +7,49 @@
 # запускаем асинхронные функцию работающие в отдельном потоке
 # ...
 
+import time 
+import pyotp 
+import qrcode 
+import io
 
-# Делаем 2FA
-# import time 
-# import pyotp 
-# import qrcode 
-  
-# from src.config import config
+from src.config import config, settings
 
-# key = config.SECRET_KEY
-  
-# uri = pyotp.totp.TOTP(key).provisioning_uri( 
-#     name='Dwaipayan_Bandyopadhyay', 
-#     issuer_name='GeeksforGeeks') 
-  
-# print(uri) 
-  
-  
-# qrcode.make(uri).save("qr.png") 
-    
-# totp = pyotp.TOTP(key) 
-  
-# while True: 
-#   print(totp.verify(input(("Enter the Code : "))))
+
+class TwoFactor:
+    def __init__(
+        self, 
+        secret_key: str = config.SECRET_KEY
+    ) -> None:
+        self.secret_key = secret_key
+        self.totp = pyotp.TOTP(self.secret_key)
+
+    async def generate_qr(
+        self, 
+        login: str, 
+        issuer_name: str = settings.app.name
+    ) -> bytes:
+        """
+        Генерирует QR-код в виде байтового объекта.
+        :param username: Логин пользователя для привязки.
+        :param issuer_name: Название издателя.
+        :return: Байтовое представление PNG-изображения QR-кода.
+        """
+        uri = self.totp.provisioning_uri(
+            name=login,
+            issuer_name=issuer_name
+        )
+
+        qr_img = qrcode.make(uri)
+        buffer = io.BytesIO()
+        qr_img.save(buffer, format="PNG")
+        buffer.seek(0)
+
+        return buffer.read()
+
+    async def verify_code(self, code: str) -> bool:
+        """
+        Проверяет, является ли переданный код корректным.
+        :param code: Код для проверки.
+        :return: True, если код корректен; иначе False.
+        """
+        return self.totp.verify(code)
