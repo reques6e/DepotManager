c9e8d1c0d050a81f6e1c06d377ba6a83053d8bb9
reques6e <voronin99borja61275y@rambler.ru>
2024-12-24 14:54:11 +0300

    Update router.py

--- api/account/router.py
+++ api/account/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, Query, HTTPException, status
 from fastapi.responses import JSONResponse
+from fastapi.requests import Request
 
 from authx import RequestToken
 from src.auth import security, get_current_token
@@ -10,15 +11,31 @@ router = APIRouter(
 )
 
 @router.get(
+    path='/test',
+    status_code=status.HTTP_200_OK,
+    description='Выдаёт тестовый ключ'
+)
+async def Generate_test_token() -> JSONResponse:
+    """
+    Тестовый метод
+
+    Выдаёт токен для работы с API
+    """
+    tk = security.create_access_token(uid='12312312')
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={'message': 'Тестовый токен', 'token': tk}
+    )
+
+@router.get(
     path='/authorization',
     status_code=status.HTTP_200_OK,
     description='Авторизация по логину и хешу пароля',
-    dependencies=[
-        Depends(security.get_token_from_request)
-    ] 
 )
 async def GetAccount(
-    current_user: RequestToken = Depends(get_current_token), 
+    request: Request,  # Передаем Request напрямую
+    current_user=Depends(get_current_token),
     target_user: int | None = Query(
         default=None,
         description='ID целевого пользователя (для администраторов или пользователей, имеющих права)'
@@ -27,12 +44,12 @@ async def GetAccount(
     """
     Получаем данные пользователя по токену.
 
+    :param request: HTTP-запрос.
     :param current_user: Данные текущего авторизованного пользователя, извлечённые из токена.
     :param target_user: ID целевого пользователя (для администраторов или пользователей, имеющих права).
     :return: Данные текущего пользователя или информация о целевом пользователе.
     """
 
-    # Если передан target_user, добавляем дополнительную логику
     if target_user:
         return JSONResponse(
             status_code=status.HTTP_200_OK,
@@ -42,8 +59,7 @@ async def GetAccount(
             }
         )
 
-    # Возвращаем данные текущего пользователя
     return JSONResponse(
         status_code=status.HTTP_200_OK,
-        content={"message": "Доступ разрешён", "user": current_user}
+        content={"message": "Доступ разрешён"}
     )
