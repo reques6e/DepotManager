3f630dc2dec3ec104e0ddbb62ffb82e38e7ca9be
reques6e <jeeeerryyfine@mail.ru>
2025-01-24 17:33:42 +0300

    Перенёс логгер в src

--- src/logger.py
+++ src/logger.py
@@ -10,19 +10,21 @@ logHandler = logging.StreamHandler()
 class CustomJsonFormatter(jsonlogger.JsonFormatter):
     def add_fields(self, log_record, record, message_dict):
         super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
-        if not log_record.get("timestamp"):
-            now = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-            log_record["timestamp"] = now
-        if log_record.get("level"):
-            log_record["level"] = log_record["level"].upper()
+        if not log_record.get('timestamp'):
+            now = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            log_record['timestamp'] = now
+        if log_record.get('level'):
+            log_record['level'] = log_record['level'].upper()
         else:
-            log_record["level"] = record.levelname
+            log_record['level'] = record.levelname
 
 
 formatter = CustomJsonFormatter(
-    "%(timestamp)s %(level)s %(message)s %(module)s %(funcName)s"
+    '%(timestamp)s %(level)s %(message)s %(module)s %(funcName)s'
 )
+# fileHandler = logging.FileHandler('{0}/{1}.log'.format(logPath, fileName))
 
 logHandler.setFormatter(formatter)
 _logger.addHandler(logHandler)
+# _logger.addHandler(fileHandler)
 _logger.setLevel(logging.INFO)
